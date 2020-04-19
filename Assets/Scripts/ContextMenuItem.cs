256c1a10d7609def1871b7e0fd23c0353a7d3c36
nburgmeijer <nick.burgmeijer@nhlstenden.com>
2020-04-19 21:20:55 +0200

    Refactored code and implemented buttons

--- Assets/Scripts/ContextMenuItem.cs
+++ Assets/Scripts/ContextMenuItem.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ContextMenuItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ContextMenuItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     public enum ItemType
     {
@@ -11,7 +12,9 @@ public class ContextMenuItem : MonoBehaviour, IPointerEnterHandler, IPointerExit
         subItem
     }
 
+    private ContextMenuItem _mainMenuContextMenuItem;
     private Image _itemBackground;
+    private ColorBlock _colorBlock;
     private ContextMenuItem _itemHit = null;
     private GraphicRaycaster _graphicRaycaster;
     private List<RaycastResult> _raycastResults = new List<RaycastResult>();
@@ -23,16 +26,22 @@ public class ContextMenuItem : MonoBehaviour, IPointerEnterHandler, IPointerExit
     [SerializeField]
     private ItemType _itemType;
 
+    public static event EventHandler OnClickItem;
+
     void Start()
     {
+        _colorBlock = this.GetComponent<Button>().colors;
+        if(_mainMenuItem != null)
+        {
+            _mainMenuContextMenuItem = _mainMenuItem.GetComponent<ContextMenuItem>();
+        }
+        
         _graphicRaycaster = this.GetComponentInParent<GraphicRaycaster>();
         _itemBackground = this.GetComponent<Image>();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
-    { 
-        //when entering any item we set the selectcolor
-        _itemBackground.color = new Color(1f, 0.5f, 0.5f, 0.6f);
+    {
         //if the mousepointer enters a menuItem that has a subMenu attached to it, open it
         if (_subMenu != null)
         {
@@ -49,51 +58,65 @@ public class ContextMenuItem : MonoBehaviour, IPointerEnterHandler, IPointerExit
             _itemHit = raycastResult.gameObject.GetComponent<ContextMenuItem>();
         }
         _raycastResults.Clear();
+
         //If the mousepointer is outside a menu
         if (_itemHit == null)
         {
-            //always set the color to deselectcolor
-            _itemBackground.color = new Color(1f, 1f, 1f, 1f);
             //If we exit from the mainmenu while a submenu was open, close it
             if (_subMenu != null)
             {
                 _subMenu.SetActive(false);
             }
-            //if we exit from a submenu, close it and set the associated MainMenuItem deselectcolor
+            //if we exit from a submenu, close it
             if (_itemType == ItemType.subItem)
             {
-                if(_mainMenuItem.GetComponent<ContextMenuItem>() != null)
-                {
-                    _mainMenuItem.GetComponent<ContextMenuItem>()._itemBackground.color = new Color(1f, 1f, 1f, 1f);
-                    gameObject.transform.parent.gameObject.SetActive(false);
-                }        
+                    transform.parent.gameObject.SetActive(false);
+                    _mainMenuItem.GetComponent<Button>().interactable = true;
             }
         }
+
         //The mouspointer exited to another item
         else
         {
-            //if the next item that will be entered is a mainMenuItem or the current item we exit is a subMenuItem
-            if (_itemHit._itemType == ItemType.mainItem || _itemType == ItemType.subItem)
+            //Pointer went to a mainItem
+            if(_itemHit._itemType == ItemType.mainItem)
             {
-                //set deselectcolor of the item we're exiting
-                _itemBackground.color = new Color(1f, 1f, 1f, 1f);
-                //if the mousepointer exited from a mainItem with a subMenu, close the submenu
-                if(_subMenu != null)
+                //From mainItem to another mainItem
+                if(_itemType == ItemType.mainItem)
                 {
-                    _subMenu.SetActive(false);
+                    if(_subMenu != null) 
+                    { 
+                        _subMenu.SetActive(false); 
+                    }
                 }
+                //from subItem to mainItem
+                if(_itemType == ItemType.subItem)
+                {
+                    transform.parent.gameObject.SetActive(false);
+                    _mainMenuItem.GetComponent<Button>().interactable = true;
+                }
+                               
             }
-            //if the mousepointer exits from a subMenuItem and enters a mainItem, close the subMenu
-            if(_itemType == ItemType.subItem && _itemHit._itemType == ItemType.mainItem)
+            if(_itemHit._itemType == ItemType.subItem && _subMenu != null)
             {
-                _itemBackground.color = new Color(1f, 1f, 1f, 1f);
-                if(_mainMenuItem.GetComponent<ContextMenuItem>() != null)
-                {
-                _mainMenuItem.GetComponent<ContextMenuItem>()._itemBackground.color = new Color(1f, 1f, 1f, 1f);
-                gameObject.transform.parent.gameObject.SetActive(false);
-                }
+                GetComponent<Button>().interactable = false;
             }
             _itemHit = null;
         }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        
+        if (OnClickItem != null)
+        {
+            OnClickItem(gameObject, EventArgs.Empty);
+        }
+        
+    }
+    void Update()
+    {
+
+      
     }
 }
---
+++ Assets/Scripts/CustomEventSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomEventSystem : MonoBehaviour
+{
+    public enum EventType
+    {
+        UIMouseClick
+    }
+    void SubScribe(EventType eventType)
+    {
+
+    }
+    void UnSubscribe()
+    {
+        //TODO
+    }
+
+    private void Start()
+    {
+        ContextMenuItem.OnClickItem += ContextMenuItem_OnClickItem;
+    }
+
+    private void ContextMenuItem_OnClickItem(object sender, System.EventArgs e)
+    {
+        GameObject obj = (GameObject)sender;
+        print("we clicked on" + obj.name);
+    }
+}
